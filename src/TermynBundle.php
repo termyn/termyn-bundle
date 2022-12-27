cc232d97ce961804d949d2a0bea5085f118e15c9
Edo Barnas <iam@edobarnas.com>
2022-12-27 16:34:09 +0100

    Define `SF` bundle

---
+++ src/TermynBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace src;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+final class TermynBundle extends Bundle implements BundleInterface
+{
+}
